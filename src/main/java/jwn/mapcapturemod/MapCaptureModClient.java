1abfce470cde318bba8d9898018763ee1547c9d8
JWN <naa3000@naver.com>
2025-02-12 19:52:57 +0900

    first commit

---
+++ src/main/java/jwn/mapcapturemod/MapCaptureModClient.java
@@ -0,0 +1,10 @@
+package jwn.mapcapturemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MapCaptureModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
